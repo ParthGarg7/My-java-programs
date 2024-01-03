cf3a51a12abbe19838646738bcbc7bc92d2b9d2e
Parth Garg <parthgarg750@gmail.com>
2024-01-03 17:28:38 +0530

    variables

---
+++ variables.java
@@ -0,0 +1,20 @@
+// variables are memory containers which are used to store and use data
+// variables can be of different type 
+// variable type tell how much memory it will take and which type of data can be stored in that variable
+
+public class variables {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 50;
+        String name = "Vir hanuman";
+
+        System.out.println("a");
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(name);
+        a=b;
+        System.out.println(a);
+
+
+    }
+}
