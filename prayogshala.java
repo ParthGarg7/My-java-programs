25d6ec86cfa932be7b2202220404c09422c96aeb
Parth Garg <parthgarg750@gmail.com>
2024-01-03 16:26:30 +0530

    print on screen

---
+++ prayogshala.java
@@ -0,0 +1,7 @@
+public class prayogshala
+{
+   public static void main(String[] args) 
+   {
+    
+   }
+}
---
+++ print.java
@@ -0,0 +1,17 @@
+public class print {
+    public static void main(String[] args) {
+        System.out.print("jai shree ganesh");
+        // to print on screen we use system.out.print function in java
+        System.out.print("\njai bhole nath"); // /n will print the text in next line
+        System.out.println("\njai maa parvati");
+        // println function will add a new line character after the end of the statement
+        System.out.print("jai maa saraswati");
+    }
+}
+/*
+ * output of the following code will be
+ * jai shree ganesh
+ * jai bhole nath
+ * jai maa parvati
+ * jai maa saraswati
+ */
