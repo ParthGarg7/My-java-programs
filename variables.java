3ef13694764228e3aef63cae2032534abe456242
Parth Garg <parthgarg750@gmail.com>
2024-01-03 18:00:04 +0530

    Variables

--- variables.java
+++ variables.java
@@ -6,15 +6,22 @@ public class variables {
     public static void main(String[] args) {
         int a = 10;
         int b = 50;
-        String name = "Vir hanuman";
+        String name = "Vir hanuman"; // S must be in uppercase
 
-        System.out.println("a");
+        System.out.println("a"); // this will not print the value of the variable
         System.out.println(a);
         System.out.println(b);
         System.out.println(name);
-        a=b;
+        a = b; // values of the variable can be changed
         System.out.println(a);
-
-
     }
 }
+
+/*
+ * output -
+ * a
+ * 10
+ * 50
+ * Vir hanuman
+ * 50
+ */
