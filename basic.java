6f481d3db070986d41aefd40505167b6eafee203
Parth Garg <parthgarg750@gmail.com>
2024-01-03 16:01:50 +0530

    updted file

---
+++ basic.java
@@ -0,0 +1,11 @@
+// we create a class in java to write any code
+public class basic  // name of class and name of file must be same
+{
+   public static void main(string args[]) // compiler will search for the main function to start
+   {
+
+   }
+}
+
+// this written code is known as boilerplate code of java code 
+//because it is necessary to write any java code 
