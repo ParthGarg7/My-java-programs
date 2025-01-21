c0523037770159f7ee8e6fcc376d43d81a18d107
Parth Garg <parthgarg750@gmail.com>
2025-01-21 09:37:44 +0530

    class learning

---
+++ Class lab/Lab 2/Tarea.java
@@ -0,0 +1,7 @@
+public class Tarea{
+	public static void main(String[] args){
+		double b = Double.parseDouble(args[0]);
+	        double h = Double.parseDouble(args[1]);
+		System.out.println("Area is " + b*h*0.5);
+	}
+ }
