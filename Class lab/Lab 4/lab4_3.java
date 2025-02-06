5995bebafa37b7e523fa8be570b42e46709f0276
Parth Garg <parthgarg750@gmail.com>
2025-02-06 11:03:20 +0530

    lab4_3

---
+++ Class lab/Lab 4/lab4_3.java
@@ -0,0 +1,37 @@
+
+public class lab4_3{
+    public static void main(String[] args) {
+     
+     Calculator cal = new Calculator();
+     System.out.println(cal.add(5, 7));
+     System.out.println(cal.add(15.89, 7.33));
+     System.out.println(cal.add(5, 7.33));
+     System.out.println(cal.add(5, 7, 8));
+     System.out.println(cal.add("Parth ", "Garg"));
+
+    }
+}
+
+class Calculator{
+
+    int add(int num1 ,int num2)
+    {
+        return  num1 + num2;
+    }
+
+    int add(int num1 ,int num2 , int num3)
+    {
+        return  num1 + num2 + num3;
+    }
+
+    double add(double num1 , double num2)
+    {
+        return  num1 + num2;
+    }
+
+    String add(String s1 , String s2)
+    {
+       return s1 + s2;
+    }
+}
+
