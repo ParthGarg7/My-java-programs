c174c52a1124b6d4bf6469978b9d88c690178f33
Parth Garg <parthgarg750@gmail.com>
2025-02-10 19:54:56 +0530

    If else in java

---
+++ Class lab/Lab 2/lab2_4.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class lab2_4{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int num = scan.nextInt();
+        if (num > 0) 
+            System.out.println(num + " is a positive number");
+        else if (num == 0)
+            System.out.println(num + " is zero");
+        else 
+            System.out.println(num + " is a negative number");
+        
+
+    }
+}
