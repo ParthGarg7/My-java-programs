03f617bf571513d5cf3e92b48712eef74c7c36ba
Parth Garg <parthgarg750@gmail.com>
2025-02-10 19:43:08 +0530

    Simple intrest using command line

---
+++ Class lab/Lab 2/lab2_2_2.java
@@ -0,0 +1,12 @@
+public class lab2_2_2 
+{
+   public static void main(String[] args)
+   {
+     double p = Double.parseDouble(args[0]);
+     double r = Double.parseDouble(args[1]);
+	  double t = Double.parseDouble(args[2]);
+	  double SI = p*r*t/100;
+	  System.out.println(SI);
+   }
+}
+
