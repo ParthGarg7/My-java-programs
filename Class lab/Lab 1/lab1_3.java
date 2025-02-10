a4baa1cfa680346a3303ce6585262f708acf4d76
Parth Garg <parthgarg750@gmail.com>
2025-02-10 21:40:51 +0530

    Grade cade

---
+++ Class lab/Lab 1/lab1_3.java
@@ -0,0 +1,19 @@
+public class lab1_3 
+{
+    public static void main(String[] args)
+     {
+        String name = "Parth Garg";
+        String rollNumber = "R2142231540";
+        String sapId = "500123456";
+        String result = "Pass";
+
+        System.out.println("********************************************");
+        System.out.println("*             GRADE SHEET                  *");
+        System.out.println("********************************************");
+        System.out.println("* Name        : " + name + "                 *");
+        System.out.println("* Roll Number : " + rollNumber + "                *");
+        System.out.println("* SAP ID      : " + sapId + "                  *");
+        System.out.println("* Result      : " + result + "                       *");
+        System.out.println("********************************************");
+    }
+}
