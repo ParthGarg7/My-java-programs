fafd406ac97816c99bf88b1e851250c0474acdb7
Parth Garg <parthgarg750@gmail.com>
2025-02-10 21:40:27 +0530

    Printing in java

---
+++ Class lab/Lab 1/lab1_2.java
@@ -0,0 +1,7 @@
+public class lab1_2{
+    public static void main(String[] args){
+        System.out.println("My Name is Parth Garg");
+        System.out.println("My Sap ID is 500122496");
+        System.out.println("My Address is Zenith boys hostel");
+    }
+}
