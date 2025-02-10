999e4dbcae34dad3c750f2fec9a916c7c9dea1b1
Parth Garg <parthgarg750@gmail.com>
2025-02-10 19:33:49 +0530

    Area using command line

---
+++ Class lab/Lab 2/lab2_1_3.java
@@ -0,0 +1,8 @@
+public class lab2_1_3{
+	public static void main(String[] args)
+	{
+		double b = Double.parseDouble(args[0]);
+	    double h = Double.parseDouble(args[1]);
+		System.out.println("Area is " + b*h*0.5);
+	}
+ }
