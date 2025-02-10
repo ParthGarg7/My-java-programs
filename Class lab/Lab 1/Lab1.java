0858f43bc2f506b21944a8159fb3c030791953f5
Parth Garg <parthgarg750@gmail.com>
2025-02-10 21:21:28 +0530

    Lab1

--- Class lab/Lab 1/Lab1.java
+++
@@ -1,7 +0,0 @@
-public class Lab1{
-    public static void main(String args[]){
-       System.out.println("helloUPES.java");
-       System.out.println("My name is Parth Garg and i am currently living in Dehradhun");
-    }
-
-}
