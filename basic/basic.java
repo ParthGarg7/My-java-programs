1ba061db0e540b82dde755e989d687363a88b472
Parth Garg <parthgarg750@gmail.com>
2025-02-13 01:34:59 +0530

    learning java

---
+++ basic/basic.java
@@ -0,0 +1,12 @@
+// we create a class in java to write any code 
+public class basic //name of class and name of file must be same
+{
+
+    public static void main(String[] args) // compiler will search for the main function to starting compiling the code
+    {
+
+    }
+}
+
+// this written code is known as boilerplate code of java code 
+//because it is necessary to write any java code 
--- printing.java
+++
@@ -1,8 +0,0 @@
-public class printing{
-    public static void main(String[] args) {
-        System.out.println("* * * *");
-        System.out.println("* * *");
-        System.out.println("* *");
-        System.out.println("*");
-    }
-}
