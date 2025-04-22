d69523d0a82991633cddac5b0b8e65cc36b19e8d
Parth Garg <parthgarg750@gmail.com>
2025-04-23 01:57:48 +0530

    Exp1

--- Class lab/Lab 8/Exp1.java
+++ Class lab/Lab 8/Exp1.java
@@ -1,32 +1,38 @@
-
 public class Exp1 {
 
-    public static void main(String[] args) {
-        Library.Book b1 = new Library.Book("Math", "unknown ", "DFGH4567G2K4V");
-        b1.displayDetails();
-    }
-}
+    // Static nested Book class
+    public static class Book {
+        private String title;
+        private String author;
 
-class Library {
-
-    static class Book {
-
-        String title;
-        String author;
-        String ISBN;
-
-        Book(String a, String b, String c) {
-            title = a;
-            author = b;
-            ISBN = c;
-
+        // Constructor
+        public Book(String title, String author) {
+            this.title = title;
+            this.author = author;
         }
 
+        // Method to display book details
         public void displayDetails() {
             System.out.println("Title: " + title);
             System.out.println("Author: " + author);
-            System.out.println("ISBN: " + ISBN);
+            System.out.println("-------------------------");
         }
     }
 
+    // Main method
+    public static void main(String[] args) {
+        // Creating Book instances with Indian authors
+        Book book1 = new Book("Malgudi Days", "R. K. Narayan");
+        Book book2 = new Book("The God of Small Things", "Arundhati Roy");
+        Book book3 = new Book("Gitanjali", "Rabindranath Tagore");
+        Book book4 = new Book("The Immortals of Meluha", "Amish Tripathi");
+        Book book5 = new Book("The Namesake", "Jhumpa Lahiri");
+
+        // Displaying book details
+        book1.displayDetails();
+        book2.displayDetails();
+        book3.displayDetails();
+        book4.displayDetails();
+        book5.displayDetails();
+    }
 }
