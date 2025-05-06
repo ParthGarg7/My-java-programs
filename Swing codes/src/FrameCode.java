e665884fd4cd7c1beb55ae056462e3ae0a0bac3e
Parth Garg <parthgarg750@gmail.com>
2025-05-06 19:18:43 +0530

    Succesfull Jframe

--- Swing codes/src/FrameCode.java
+++ Swing codes/src/FrameCode.java
@@ -1,7 +1,13 @@
+import java.awt.Frame;
 import javax.swing.JFrame;
 
 public class FrameCode{
     public static void main(String[] args) {
         JFrame f1 = new JFrame("hi");   
+
+        f1.setSize(300,300);
+        f1.setVisible(true);
+        f1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f1.setLocationRelativeTo(null);
     }
 }
