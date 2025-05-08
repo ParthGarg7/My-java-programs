e870d06e37e36afb74355489f47f8293e55a1d8c
Parth Garg <parthgarg750@gmail.com>
2025-05-08 20:01:47 +0530

    Collections

---
+++ My Java Programs/Collections/src/SortingEx1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class SortingEx1 {
+      public static void main(String[] args){
+        List<Integer> L1 = new ArrayList<>(List.of(8,9,22,0,1,2,2,4,3,9,23,422,28,34,87));
+        
+        Collections.sort(L1);
+
+        System.out.println(L1);
+
+
+
+      }
+}
---
+++ My Java Programs/Collections/src/SortingEx2.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class SortingEx2 {
+      public static void main(String[] args){
+      
+        List<String> L1 = new ArrayList<>(List.of("Parth","Ram", "Garg")); 
+        
+        Collections.sort(L1);
+
+        System.out.println(L1);
+
+      }
+}
