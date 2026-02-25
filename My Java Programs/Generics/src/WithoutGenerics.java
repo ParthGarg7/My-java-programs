b70c5f72d5a75ddfb0c8a23ab39caa8844dca62b
vedantshukla3512 <shuklavedant2101@gmail.com>
2026-02-25 18:49:56 +0530

    reverting the changes

--- My Java Programs/Generics/src/WithoutGenerics.java
+++ My Java Programs/Generics/src/WithoutGenerics.java
@@ -1,1 +1,26 @@
-fghjkl;'
+
+public class WithoutGenerics {
+
+    public static void main(String[] args) {
+        Add a1 = new Add(3, 4);
+
+        System.out.print(a1.ReturnSum());
+    }
+}
+
+class Add {
+
+    public int a;
+    public int b;
+
+    public Add(int a, int b) {
+        this.a = a;
+        this.b = b;
+
+    }
+
+    public int ReturnSum() {
+        return a + b;
+    }
+
+}
